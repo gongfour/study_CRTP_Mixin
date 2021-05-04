ea63aa1255282ab4fc4abeb705729470103ac290
wonjin <kangwonjin.dev@gmail.com>
2021-05-04 23:29:09 +0900

    add a example of CRTP

--- app_crtp.cpp
+++ app_crtp.cpp
@@ -1,5 +1,56 @@
+
+#include <string>
+#include <iostream>
+
+// Original Class
+class Name
+{
+    std::string m_firstName;
+    std::string m_lastName;
+
+public:
+    Name(std::string firstName, std::string lastName)
+        : m_firstName(firstName)
+        , m_lastName(lastName)
+    {
+    }
+
+    void print() const
+    {
+        std::cout << m_lastName << ", " << m_firstName << '\n';
+    }
+};
+
+// This is the CRTP
+template <class Printable>
+struct RepeatPrint : Printable // public inheritance (STRUCT)
+{
+    // copy constructor or move constructor
+    explicit RepeatPrint(const Printable& printable) : Printable(printable) 
+    {}
+
+    void repeat(unsigned int n) const
+    {
+        while(n-- > 0)
+            this->print();
+    }
+};
+
+// It is useful
+template <class Printable>
+RepeatPrint<Printable> repeatPrint(const Printable& printable)
+{
+    return RepeatPrint<Printable>(printable);
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    Name ned("Eddard", "Stark");
+    repeatPrint(ned).repeat(10);
+    repeatPrint(Name{"A", "B"}).repeat(2);
+
+    RepeatPrint<Name> print(ned);
+    print.repeat(5);
+    print.print();
     return 0;
 }
